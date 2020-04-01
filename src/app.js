bf9b6c81e2ecee44d9a498b49bb5b38d5cac8672
Hsien <hsien-hsiu.liao@yandex.com>
2020-04-01 06:56:13 -0700

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 App = {
-    
+    load: async () => {
+        //load app....
+        console.log("app loading...")
+    }
+
+    $(() => {
+        $(window).load(() => {
+            App.load()
+        })
+    })
 }
